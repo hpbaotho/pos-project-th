58648126fd7fc93d99c67c2f66114c468dce9793
phurin.boom@gmail.com <phurin.boom@gmail.com@5266ce41-c766-9d82-00e6-7b53161e4a53>
2014-11-19 15:44:08 +0000

    Able Execute return data set ja :)

---
+++ trunk/Application/POS/POS.Application/TestByBoom.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using POS.Service.Services;
+
+namespace POS
+{
+    public partial class TestByBoom : Form
+    {
+        public TestByBoom()
+        {
+            InitializeComponent();
+        }
+
+        private void TestByBoom_Load(object sender, EventArgs e)
+        {
+            DataSet ds = ServiceProvider.SystemConfigurationService.GetSystemConfig();
+            if (ds != null)
+            {
+                textBox1.Text = ds.Tables[0].Rows[0]["system_configuration_code"].ToString();
+                textBox2.Text = ds.Tables[0].Rows[0]["system_configuration_name"].ToString();
+            }
+        }
+
+        private void btnUpdate_Click(object sender, EventArgs e)
+        {
+
+        }
+
+    }
+}
---
+++ trunk/Application/POS/POS.Service/Services/Service/SystemConfigurationService.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using POS.BL.Entities.Entity;
+using POS.Service.Services;
+using System.Data;
+using System.Data.Common;
+
+namespace POS.Service.Services.Service
+{
+    public class SystemConfigurationService : ServiceBase<SystemConfiguration>
+    {
+        public DataSet GetSystemConfig()
+        {
+            StringBuilder sql = new StringBuilder();
+            sql.AppendLine(@"SELECT * FROM cbs_system_configuration
+                            WHERE system_configuration_code = '0101'  ");
+
+            IList<DbParameter> parameters = new List<DbParameter>();
+            //parameters.Add(base.CreateParameter("@Code", "0101"));
+
+            return base.ExecuteQuery(sql.ToString(), parameters.ToArray());
+        }
+    }
+}
