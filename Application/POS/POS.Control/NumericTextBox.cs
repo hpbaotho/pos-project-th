3c57eab78e0f3d67ca62407633747dac7c73f359
lameloura <lameloura@gmail.com>
2015-01-17 05:17:09 +0000

    Standard-Input Number Only

---
+++ Application/POS/POS.Control/NumericTextBox.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace POS.Control
+{
+    public partial class NumericTextBox : TextBox//  UserControl
+    {
+        public NumericTextBox()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnKeyPress(KeyPressEventArgs e)
+        {
+           
+            base.OnKeyPress(e);
+
+            //exploit the e.handled
+            if (!char.IsControl(e.KeyChar) && !char.IsDigit(e.KeyChar))
+                e.Handled = true;
+
+        }
+
+    }
+}
